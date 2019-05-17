3bf192e77fa8fbd5a167995750fc50b170662168
Rodolpho Lobo <rodolpho1704@gmail.com>
2019-05-17 01:32:17 -0300

    create home controller and home#index template

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,6 +1,9 @@
 class Gallery
   include Mongoid::Document
   field :name, type: String
+  field :gallery_cover, type: String
+
+  mount_uploader :gallery_cover, PhotographUploader
 
   has_many :images
 end
